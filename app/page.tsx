a167ef18a7cb609c42097f95b1903a2ac2e5bb06
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2023-08-14 19:17:00 -0400

    feat: added test endpoint

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function MainPage() {
   const [userUrl, setUserUrl] = useState("");
   const [shortUrlId, setShortUrlId] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const baseUrl = useRef("")
+  const baseUrl = useRef("");
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setUserUrl(event.target.value);
@@ -24,7 +24,6 @@ export default function MainPage() {
           url: userUrl,
         }),
         cache: "no-store",
-        mode: "cors",
       });
 
       const url = await response.json();
@@ -45,14 +44,32 @@ export default function MainPage() {
     }
   };
 
+  const testGet = async () => {
+    try {
+      const response = await fetch(`/api/test`, {
+        method: "GET",
+        cache: "no-store",
+      });
+
+      const formattedResponse = await response.json();
+
+      console.log(formattedResponse);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
-    baseUrl.current = window.location.href
+    baseUrl.current = window.location.href;
   }, []);
 
   return (
     <main className="w-screen h-screen flex flex-col justify-center items-center gap-16 pb-16 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-900 via-gray-950 to-black">
       <div className="flex flex-col gap-16 items-center">
-        <h1 className="font-extrabold text-transparent text-center text-5xl sm:text-7xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+        <h1
+          className="font-extrabold text-transparent text-center text-5xl sm:text-7xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
+          onClick={testGet}
+        >
           Nora&apos;s URL shortener
         </h1>
         <div className="flex flex-col justify-center items-center gap-4 w-full px-12 max-w-2xl">
